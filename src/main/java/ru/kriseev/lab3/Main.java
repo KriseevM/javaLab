3f1897e09cf2e3885ce70a77e89399170cc323df
KriseevM <mish.kryseev@gmail.com>
2022-09-15 18:53:24 +0300

    Entry point

---
+++ src/main/java/ru/kriseev/lab3/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ru.kriseev.lab3;
+
+/**
+ *
+ * @author k17
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
