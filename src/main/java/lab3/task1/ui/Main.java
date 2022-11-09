a16a5132bbd49558c45054741d9675625a4d5d4a
Kriseev Mikhail <34946346+KriseevM@users.noreply.github.com>
2022-11-09 17:01:33 +0300

    Add jfx support

---
+++ src/main/java/lab3/task1/ui/Main.java
@@ -0,0 +1,18 @@
+package lab3.task1.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+
+}
