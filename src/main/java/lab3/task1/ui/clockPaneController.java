e18b6379afd6530639b707ccfdb1c2e4703e7583
Kriseev Mikhail <34946346+KriseevM@users.noreply.github.com>
2022-11-09 17:06:10 +0300

    Temporary UI clockPane

---
+++ src/main/java/lab3/task1/ui/clockPaneController.java
@@ -0,0 +1,21 @@
+package lab3.task1.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class clockPaneController {
+
+    @FXML
+    private TextField brandNameText;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TextField priceText;
+
+    @FXML
+    private TextField timeText;
+
+}
